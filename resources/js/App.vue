efc79f2e2bc4c2299c7cdbd8afe61ae352ec00f3
Mariano Montero <gibran.colio@gmail.com>
2024-06-24 10:11:02 -0600

    Se añadió vue a proyecto

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+<script lang='ts'>
+import { ref} from 'vue'
+
+</script>
+<template>
+    <div></div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+        },
+    },
 });
