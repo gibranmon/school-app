afca9f6410b89ef1b04c4b9ed29895be024b8fe8
Mariano Montero <gibran.colio@gmail.com>
2024-06-24 17:35:56 -0600

    Definicion de rutas para maestros y alumnos enrolados

--- resources/js/router/router.ts
+++ resources/js/router/router.ts
@@ -5,6 +5,16 @@ const routes = [
         path: '/',
         name: 'home',
         component: () => import("../views/HomeView.vue")
+    },
+    {
+        path: '/teacher/:id',
+        name: 'teacher_detail',
+        component: () => import("../modules/teachers/views/TeacherDetail.vue")
+    },
+    {
+        path: '/teacher/lesson/:id',
+        name: 'enrrolled_students',
+        component: () => import('../modules/enrroll/views/EnrrollStudents.vue')
     }
 ];
 
